a3ac0d77f74f8809bce3ece1fbe44472fbbae246
Dan Vasques <dan.vasques@outlook.com.br>
2024-10-22 14:13:47 -0300

    estruturação do algoritmo que calcula salario de funcionario.

--- src/exerciciosjava/SalarioFuncionario.java
+++ src/exerciciosjava/SalarioFuncionario.java
@@ -1,9 +1,38 @@
 /*Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por
 hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas
 decimais. */
-
 package src.exerciciosjava;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class SalarioFuncionario {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        int idFuncionario, horasTrabalhadas;
+        double valorHora, salario;
+
+        try (Scanner sc = new Scanner(System.in)) {
+
+            System.out.print("Digite o ID do funcionário: ");
+            idFuncionario = sc.nextInt();
+
+            System.out.print("Digite o número de horas trabalhadas: ");
+            horasTrabalhadas = sc.nextInt();
+
+            System.out.print("Digite o valor que recebe por hora: ");
+            valorHora = sc.nextDouble();
+
+            salario = horasTrabalhadas * valorHora;
+
+            System.out.println("NUMBER = " + idFuncionario);
+            System.out.printf("SALARY = U$ %.2f", salario);
+
+        }
+
+    }
 
 }
