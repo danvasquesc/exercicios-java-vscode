9b940425f2f1798237fdb79ed39214b769f137bf
Dan Vasques <dan.vasques@outlook.com.br>
2024-10-24 16:22:58 -0300

    algoritmo que fará troca de variaveis com valores ja declarados no inicio, usando variavel temporaria

--- src/exerciciosinternet/atribuicoesVariaveis.java
+++ src/exerciciosinternet/atribuicoesVariaveis.java
@@ -1,5 +1,31 @@
+/*Escreva um algoritmo que armazene o valor 10 em uma variável A e o valor 20 em uma variável B.
+A seguir (utilizando apenas atribuições entre variáveis) troque os seus conteúdos fazendo com que o
+valor que está em A passe para B e vice-versa. Ao final, escrever os valores que ficaram armazenados
+nas variáveis. */
+
+
 package src.exerciciosinternet;
 
 public class atribuicoesVariaveis {
+
+    public static void main(String[] args) {
+
+        int A = 10;
+        int B = 20;
+        int variavelTemporaria;
+
+        System.out.println("Antes da troca:");
+        System.out.println("A = " + A);
+        System.out.println("B = " + B);
+
+        variavelTemporaria = A;   //alocar o valor temporariamente pro A não ficar perdido
+        A = B;
+        B = variavelTemporaria;
+
+        System.out.println("Depois da troca:");
+        System.out.println("A = " + A);
+        System.out.println("B = " + B);
+
+    }
 
 }
