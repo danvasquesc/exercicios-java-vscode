2b4d838d4df8425b3cbd087c5b21f2b5fd836896
Dan Vasques <dan.vasques@outlook.com.br>
2024-10-24 16:31:14 -0300

    criação de algoritmo que apresenta o valor antecessor

---
+++ src/exerciciosinternet/numeroAntecessor.java
@@ -0,0 +1,25 @@
+/*Escreva um algoritmo para ler um valor (do teclado) e escrever (na tela) o seu antecessor */
+package src.exerciciosinternet;
+
+import java.util.Scanner;
+
+public class numeroAntecessor {
+
+    public static void main(String[] args) {
+
+        int valorInicial, valorAntecessor;
+
+        try (Scanner sc = new Scanner(System.in)) {
+
+            System.out.print("Digite um valor: ");
+            valorInicial = sc.nextInt();
+
+            valorAntecessor = valorInicial - 1;
+
+            System.out.println("O antecessor de " + valorInicial + " é " + valorAntecessor);
+
+        }
+
+    }
+
+}
