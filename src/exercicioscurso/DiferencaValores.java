8e6ce03594410990996b2969dc9de188cb81e945
Dan Vasques <dan.vasques@outlook.com.br>
2024-10-24 15:27:43 -0300

    organização e alteração dos nomes das pastas , pois vou usar o repositorio para outros exercicios alem

--- src/exercicioscurso/DiferencaValores.java
+++ src/exercicioscurso/DiferencaValores.java
@@ -1,5 +1,5 @@
 /*Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D). */
-package src.exerciciosjava;
+package src.exercicioscurso;
 
 import java.util.Scanner;
 
--- src/exercicioscurso/Soma.java
+++ src/exercicioscurso/Soma.java
@@ -1,6 +1,6 @@
 /*Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
 mensagem explicativa, conforme exemplos.*/
-package src.exerciciosjava;
+package src.exercicioscurso;
 
 // importando a classe Scanner para entrada de dados
 import java.util.Scanner;
