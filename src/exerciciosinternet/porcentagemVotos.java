e336e74dbc8c941d72f0c42148ac7c0bcc3e048a
Dan Vasques <dan.vasques@outlook.com.br>
2024-10-24 16:37:13 -0300

    construindo algoritmo que calculará porcentagem de votos em um municipio

---
+++ src/exerciciosinternet/porcentagemVotos.java
@@ -0,0 +1,27 @@
+/* Escreva um algoritmo para ler o número total de eleitores de um município, o número de votos
+brancos, nulos e válidos. Calcular e escrever o percentual que cada um representa em relação ao total
+de eleitores.  */
+
+
+package src.exerciciosinternet;
+
+public class porcentagemVotos {
+
+    public static void main(String[] args) {
+
+        int totalEleitores;
+
+        try (Scanner sc = new Scanner(System.in)) {
+
+            System.out.print("Digite o número total de eleitores do município: ");
+            totalEleitores = sc.nextInt();
+
+
+
+
+
+        }
+
+    }
+
+}
