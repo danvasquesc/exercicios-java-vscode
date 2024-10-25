3fce95c759e8a8be7fcf19604a0c08e3c88b9dd8
Dan Vasques <dan.vasques@outlook.com.br>
2024-10-25 14:45:28 -0300

    adicionado entrada de dados e calculo das porcentagens

--- src/exerciciosinternet/porcentagemVotos.java
+++ src/exerciciosinternet/porcentagemVotos.java
@@ -1,24 +1,38 @@
 /* Escreva um algoritmo para ler o número total de eleitores de um município, o número de votos
 brancos, nulos e válidos. Calcular e escrever o percentual que cada um representa em relação ao total
 de eleitores.  */
-
-
 package src.exerciciosinternet;
+
+import java.util.Locale;
+import java.util.Scanner;
 
 public class porcentagemVotos {
 
     public static void main(String[] args) {
 
-        int totalEleitores;
+        Locale.setDefault(Locale.US);
+
+        int totalEleitores, votosBrancos, votosNulos, votosValidos;
+        double porcentagemBrancos, porcentagemNulos, porcentagemValidos;
 
         try (Scanner sc = new Scanner(System.in)) {
 
             System.out.print("Digite o número total de eleitores do município: ");
             totalEleitores = sc.nextInt();
 
+            System.out.print("Digite o número de votos brancos: ");
+            votosBrancos = sc.nextInt();
 
+            System.out.print("Digite o número de votos nulos: ");
+            votosNulos = sc.nextInt();
 
+            System.out.print("Digite o número de votos válidos: ");
+            votosValidos = sc.nextInt();
 
+            /*transforma int em double para resultado sair em double*/
+            porcentagemBrancos = ((double) votosBrancos / totalEleitores) * 100;
+            porcentagemNulos = ((double) votosNulos / totalEleitores) * 100;
+            porcentagemValidos = ((double) votosValidos / totalEleitores) * 100;
 
         }
 
