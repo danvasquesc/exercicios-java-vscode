73f08d045af140e60e2e53d3101b912501659a84
Dan Vasques <dan.vasques@outlook.com.br>
2024-10-25 16:10:00 -0300

    finalização de algoritmo que apresenta quantos salarios minimos uma pessoa recebe. utilizacao do metodo Math.round pra arredondamento

---
+++ src/exerciciosinternet/salariosMinimosGanhos.java
@@ -0,0 +1,29 @@
+/*Crie um algoritmo que leia o valor do salário mínimo e o valor do salário de um usuário,
+calcule a quantidade de salários mínimos esse usuário ganha e imprima o resultado.
+(1SM=R$788,00) */
+package src.exerciciosinternet;
+
+import java.util.Scanner;
+
+public class salariosMinimosGanhos {
+
+    public static void main(String[] args) {
+
+        double salarioMinimo = 788.00;
+        double salarioUsuario;
+        int qntdSalario;
+
+        try (Scanner sc = new Scanner(System.in)) {
+
+            System.out.print("Digite o valor do seu salário: ");
+            salarioUsuario = sc.nextDouble();
+
+            qntdSalario = (int) Math.round(salarioUsuario / salarioMinimo);/*arredondamento pra mais */
+
+            System.out.println("A quantidade de salários mínimos recebido é " + qntdSalario);
+
+        }
+
+    }
+
+}
