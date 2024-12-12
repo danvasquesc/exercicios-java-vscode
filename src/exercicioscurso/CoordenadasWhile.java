18b3c1059f6bc95e6c8db5d083fa3a4fbc2092a9
Dan Vasques <dan.vasques@outlook.com.br>
2024-12-12 13:30:21 -0300

    coloquei o exercicio no programa pra acompanhar

--- src/exercicioscurso/CoordenadasWhile.java
+++ src/exercicioscurso/CoordenadasWhile.java
@@ -1,3 +1,7 @@
+/*Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema
+cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo
+menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma). */
+
 import java.util.Scanner;
 
 public class CoordenadasWhile {
@@ -12,7 +16,10 @@ public class CoordenadasWhile {
             x = sc.nextInt();
             y = sc.nextInt();
 
-            while (x != 0 && y != 0) { /*&& garante que as duas precisam ser verdadeiras pro while continuar, se uma das duas for falsa, ja encerra */
+            while (x != 0 && y != 0) { /*
+                                        * && garante que as duas precisam ser verdadeiras pro while continuar, se uma
+                                        * das duas for falsa, ja encerra
+                                        */
                 if (x > 0 && y > 0) {
                     System.out.println("Primeiro");
                 } else if (x < 0 && y > 0) {
