4a57e7e9b26b34bd789ce4ed6bb38cb14e7a27ca
Dan Vasques <dan.vasques@outlook.com.br>
2024-12-12 12:58:52 -0300

    programa le coordenadas de x e y, apresenta seu quadrante e encerra o while caso seja falso

--- src/exercicioscurso/CoordenadasWhile.java
+++ src/exercicioscurso/CoordenadasWhile.java
@@ -12,20 +12,21 @@ public class CoordenadasWhile {
             x = sc.nextInt();
             y = sc.nextInt();
 
-            while (x != 0 || y != 0) {
+            while (x != 0 && y != 0) { /*&& garante que as duas precisam ser verdadeiras pro while continuar, se uma das duas for falsa, ja encerra */
                 if (x > 0 && y > 0) {
                     System.out.println("Primeiro");
-                }
-                else if (x < 0 && y > 0) {
+                } else if (x < 0 && y > 0) {
                     System.out.println("Segundo");
+                } else if (x < 0 && y < 0) {
+                    System.out.println("Terceiro");
+                } else {
+                    System.out.println("Quarto");
                 }
 
+                System.out.println("Digite a coordenada de X e depois de Y");
+                x = sc.nextInt();
+                y = sc.nextInt();
             }
-
-
-
         }
-        
     }
-
 }
