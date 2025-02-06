88f8af32f26ba9b67ab4519d80c35dd61075a282
Dan Vasques <dan.vasques@outlook.com.br>
2025-02-06 12:01:29 -0300

    estruturando o programa

---
+++ src/exercicioscurso/Lista4Exercicio6.java
@@ -0,0 +1,15 @@
+/*Ler um número inteiro N e calcular todos os seus divisores. */
+
+import java.util.Scanner;
+
+public class Lista4Exercicio6 {
+
+    public static void main(String[] args) {
+
+        try (Scanner sc = new Scanner(System.in)) {
+
+        }
+
+    }
+
+}
