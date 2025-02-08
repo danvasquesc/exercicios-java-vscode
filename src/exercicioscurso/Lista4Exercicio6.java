584e1ab1f0337a4df01cba992239b19e0c47a617
Dan Vasques <dan.vasques@outlook.com.br>
2025-02-08 20:02:21 -0300

    programa calcula divisores de N

--- src/exercicioscurso/Lista4Exercicio6.java
+++ src/exercicioscurso/Lista4Exercicio6.java
@@ -6,10 +6,21 @@ public class Lista4Exercicio6 {
 
     public static void main(String[] args) {
 
+        int N, i;
+
         try (Scanner sc = new Scanner(System.in)) {
 
+            System.out.println("Digite um número inteiro");
+            N = sc.nextInt();
+
+            for (i = 1; i <= N; i++) {
+
+                if (N % i == 0) {
+                    /*verifica se N é divisível por i */
+                    System.out.println(i);
+                }
+            }
         }
 
     }
-
 }
